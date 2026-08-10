5637bb6eed61afd46cfb5a7e3d4537679a3f754a
wooest <htpapa9645@gmail.com>
2026-08-10 09:14:12 +0900

    이벤트 전파...

--- part3/vite-react-event/src/App.tsx
+++ part3/vite-react-event/src/App.tsx
@@ -39,13 +39,68 @@
 // }
 
 // 함수참조 방식으로는 매개변수를 전달할수없다 > 문제점 예
+// export default function App(){
+//   const clickHandler = (msg: string) => {
+//     alert(msg);
+//   };
+//   return(
+//     <>
+//       <button onClick={() => { clickHandler('hello'); }}>click</button>
+//     </>
+//   );
+// }
+
+
+// 이벤트 객체 > 이벤트 객체와 인라인 핸들러
+// export default function App() {
+//   return (
+//     <>
+//       <button
+//         onClick={(e) => {
+//           console.log(e);
+//         }}
+//       >
+//         click
+//       </button>
+//     </>
+//   );
+// }
+
+// 이벤트 객체와 함수 참조
+// export default function App(){
+//   const clickHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+//     console.log(e);
+//   };
+//   return(
+//     <>
+//       <button onClick={clickHandler}>click</button>
+//     </>
+//   );
+// }
+
+
+// 이벤트 객체 > 타입 추론 ... e 에 마우스를 올리면 자동으로 이벤트 객체의 타입을 추론해 보여준다.
+// export default function App(){
+//   return(
+//     <>
+//       <button onClick={ (e) => console.log(e) }>click</button>
+//     </>
+//   );
+// }
+
+
+// 이벤트 객체와 매개변수
 export default function App(){
-  const clickHandler = (msg: string) => {
+  const clickHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> ,
+    msg: string,
+  ) => {
+    console.log(e);
     alert(msg);
   };
   return(
     <>
-      <button onClick={() => { clickHandler('hello'); }}>click</button>
+      <button onClick={(e) => clickHandler(e, 'hello')}>click</button>
     </>
   );
 }
