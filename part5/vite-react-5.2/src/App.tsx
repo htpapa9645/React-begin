fe26e1b51c2d52bf415e8ac311348fa4c9057a6a
wooest <htpapa9645@gmail.com>
2026-09-03 00:54:46 +0900

    emotion...

--- part5/vite-react-5.2/src/App.tsx
+++ part5/vite-react-5.2/src/App.tsx
@@ -1,14 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Button = styled.button`
+const Button = styled.button<{ $primary?: boolean }>`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #ffccdd;
   color: #aabbcc;
   margin: 0 1em;
   padding: 0.25em 1em;
+  ${ (props) => 
+    props.$primary &&
+    css `
+        background: #bf4f74;
+        color: white;
+    `
+  }
 `;
 
 export default function App() {
-    return <Button>Click Me</Button>
+    return <Button $primary>Click Me</Button>
 }
+
+
+// 태그드 템플릿 리터럴 사용 예..
+// function tagFunction(strings, ...values) {
+//   console.log(strings); // 문자열 배열
+//   console.log(values); // 삽입된 표현식 값들
+// }
+// const name = "John";
+// const age = 25;
+
+// tagFunction`Hello, my name is ${name} and I am ${age} years old.`;
