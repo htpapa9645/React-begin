8bb5511ba4add04e293abc91eff2c9b0269c34bc
wooest <htpapa9645@gmail.com>
2026-09-04 00:04:43 +0900

    emotion...

--- part5/vite-react-5.2/src/App.tsx
+++ part5/vite-react-5.2/src/App.tsx
@@ -1,24 +1,24 @@
-import styled, { css } from "styled-components";
+// import styled, { css } from "styled-components";
 
-const Button = styled.button<{ $primary?: boolean }>`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #ffccdd;
-  color: #aabbcc;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-  ${ (props) => 
-    props.$primary &&
-    css `
-        background: #bf4f74;
-        color: white;
-    `
-  }
-`;
+// const Button = styled.button<{ $primary?: boolean }>`
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid #ffccdd;
+//   color: #aabbcc;
+//   margin: 0 1em;
+//   padding: 0.25em 1em;
+//   ${ (props) => 
+//     props.$primary &&
+//     css `
+//         background: #bf4f74;
+//         color: white;
+//     `
+//   }
+// `;
 
-export default function App() {
-    return <Button $primary>Click Me</Button>
-}
+// export default function App() {
+//     return <Button $primary>Click Me</Button>
+// }
 
 
 // 태그드 템플릿 리터럴 사용 예..
@@ -30,3 +30,42 @@ export default function App() {
 // const age = 25;
 
 // tagFunction`Hello, my name is ${name} and I am ${age} years old.`;
+
+
+// emotion 라이브러리 기본 사용법
+// import { css } from '@emotion/css';
+
+// export default function App() {
+//     return (
+//         <button
+//             className={css`
+//                 background: transparent;
+//                 border-radius: 3px;
+//                 border: 2px solid #bf4f74;
+//                 color: #bf4f74;
+//                 margin: 0 1em;
+//                 padding: 0.25em 1em;
+//             `}
+//         >클릭버튼이유~~</button>
+//     );
+// }
+
+
+// emotion 라이브러리 기본 사용법 > 동적 스타일...
+import { css } from '@emotion/css';
+
+export default function App() {
+    const isActive = true;
+    return (
+        <button
+            className={css`
+                background: ${ isActive? 'blue' : 'transparent' };
+                border-radius: 3px;
+                border: 2px solid #bf4f74;
+                color: ${ isActive? 'white' : '#bf4f74' };
+                margin: 0 1em;
+                padding: 0.25em 1em;
+            `}
+        >클릭버튼이유~~</button>
+    );
+}
