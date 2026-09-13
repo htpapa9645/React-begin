43400e973f9a77af028754fc30b29d9b4a1ef6bd
wooest <htpapa9645@gmail.com>
2026-09-13 22:06:19 +0900

    계산기 로직구현..

--- part6/calculator/src/App.tsx
+++ part6/calculator/src/App.tsx
@@ -7,27 +7,47 @@ export default function App() {
     operation: null,
     isNewNumber: true,
   });
+  // 숫자 버튼 클릭 처리 함수
+  const handleNumberClick = (
+    event : React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+      console.log(event.currentTarget.value);
+  };
+  // 연산 기호 버튼 클릭 처리 함수
+  const handleOperatorClick = (
+    event : React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+      console.log(event.currentTarget.value);
+  };
+  // C 버튼 클릭 처리 함수 : 모든 상태 초기화
+  const handleClear = () => {
+      console.log('clear');
+  };
+  // 소수점 버튼 클릭 처리 함수 : 현재 숫자에 소수점이 없을 경우에만 추가
+  const handleDot = () => {
+    console.log('dot');
+  };
   return (
       <article className="calculator">
       <form name="forms">
-        <input type="text" name="output" readOnly />
-        <input type="button" className="clear" value="C" />
-        <input type="button" className="operator" value="/" />
-        <input type="button" value="1" />
-        <input type="button" value="2" />
-        <input type="button" value="3" />
-        <input type="button" className="operator" value="*" />
-        <input type="button" value="4" />
-        <input type="button" value="5" />
-        <input type="button" value="6" />
-        <input type="button" className="operator" value="+" />
-        <input type="button" value="7" />
-        <input type="button" value="8" />
-        <input type="button" value="9" />
-        <input type="button" className="operator" value="-" />
-        <input type="button" className="dot" value="." />
-        <input type="button" value="0" />
-        <input type="button" className="operator result" value="=" />
+        <input type="text" name="output" value={state.currentNumber} readOnly />
+        <input type="button" className="clear" value="C" onClick={handleClear} />
+        <input type="button" className="operator" value="/" onClick={handleOperatorClick} />
+        <input type="button" value="1" onClick={handleNumberClick} />
+        <input type="button" value="2" onClick={handleNumberClick} />
+        <input type="button" value="3" onClick={handleNumberClick} />
+        <input type="button" className="operator" value="*" onClick={handleOperatorClick} />
+        <input type="button" value="4" onClick={handleNumberClick} />
+        <input type="button" value="5" onClick={handleNumberClick} />
+        <input type="button" value="6" onClick={handleNumberClick} />
+        <input type="button" className="operator" value="+" onClick={handleOperatorClick} />
+        <input type="button" value="7" onClick={handleNumberClick} />
+        <input type="button" value="8" onClick={handleNumberClick} />
+        <input type="button" value="9" onClick={handleNumberClick} />
+        <input type="button" className="operator" value="-" onClick={handleOperatorClick} />
+        <input type="button" className="dot" value="." onClick={handleDot} />
+        <input type="button" value="0" onClick={handleNumberClick} />
+        <input type="button" className="operator result" value="=" onClick={handleOperatorClick} />
       </form>
     </article>
   );
