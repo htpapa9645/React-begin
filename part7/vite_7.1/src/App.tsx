86e3b3ac7a7e514b03753c03988e00ffd3e3059e
wooest <htpapa9645@gmail.com>
2026-09-21 15:28:06 +0900

    변수에 담아 렌더링..

--- part7/vite_7.1/src/App.tsx
+++ part7/vite_7.1/src/App.tsx
@@ -5,3 +5,19 @@ export default function App(){
   }
   return <h1>로그인해야 합니다.</h1>;
 }
+
+export function App2() {
+  const isLogin = true;
+  let message;
+  if (isLogin) {
+    message = "환영합니다... App2";
+  } else {
+    message = "로그인해야 합니다... App2";
+  }
+  return (
+    <div>
+      <hr style={{borderColor: 'red' }} />
+      <h1>{message}</h1>
+    </div>
+  );
+}
