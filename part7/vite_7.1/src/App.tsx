38bfeb79373ebddeddb6b68c04d7abcb48de7f05
wooest <htpapa9645@gmail.com>
2026-09-21 17:17:34 +0900

    삼항 연산자를 이용한 조건부 렌더링..

--- part7/vite_7.1/src/App.tsx
+++ part7/vite_7.1/src/App.tsx
@@ -17,7 +17,63 @@ export function App2() {
   return (
     <div>
       <hr style={{borderColor: 'red' }} />
+      <h1>App2</h1>
       <h1>{message}</h1>
+    </div>
+  );
+}
+
+import Logout from "./components/Logout";
+export function App3() {
+  const isLogin = false;
+  let message;
+  if (isLogin) {
+    message = <h1>환영합니다</h1>;
+  } else {
+    message = <Logout />;
+  }
+  return <>
+          <hr style={{borderColor: 'red' }} />
+          <h1>App3</h1>
+          {message}
+         </>;
+}
+
+export function App4() {
+  const isLogin = true;
+  return (
+    <div>
+      <hr style={{borderColor: 'red' }} />
+      <h1>App4</h1>
+      <h1>{isLogin ? '환영합니다!' : '로그인해야 합니다.'}</h1>
+    </div>
+  );
+}
+
+export function App5(){
+  const isLogin = true;
+  return (
+    <div>
+      <hr style={{borderColor: 'red' }} />
+      <h1>App5</h1>
+      {
+        isLogin ? <h1 style={{color:'blue'}}>환영한다.</h1>
+                :
+                <h1 style={{color: 'red'}}>로그인해야 한다.</h1>
+      }
+    </div>          
+  );
+}
+
+import Login from "./components/Login";
+
+export function App6(){
+  const isLogin = true;
+  return (
+    <div>
+      <hr style={{borderColor: 'red' }} />
+      <h1>App6</h1>
+      {isLogin ? <Login /> : <Logout />}
     </div>
   );
 }
--- part7/vite_7.1/src/main.tsx
+++ part7/vite_7.1/src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {App2} from './App.tsx'
+import {App3} from './App.tsx'
+import {App4} from './App.tsx'
+import {App5} from './App.tsx'
+import {App6} from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <App2 />
+    <App3 />
+    <App4 />
+    <App5 />
+    <App6 />
   </StrictMode>,
 )
