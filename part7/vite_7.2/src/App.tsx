28b75036de3a7ac72d78de2c58c3730b9c7430d2
wooest <htpapa9645@gmail.com>
2026-09-25 15:35:57 +0900

    마무리...

--- part7/vite_7.2/src/App.tsx
+++ part7/vite_7.2/src/App.tsx
@@ -39,3 +39,106 @@ export function App3(){
     </div>
   ); 
 }
+
+export function App4(){
+  const items = [
+    <li key='사과'>사과</li>
+    , <li key='바나나'>바나나</li>
+    , <li key='딸기'>딸기</li>
+  ];
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App4</h1>
+      <ul>{items}</ul>
+    </div>
+  ); 
+}
+
+export function App5(){
+  const items = ['사과', '바나나', '딸기'];
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App5</h1>
+      <ul>
+        {
+          items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export function App6(){
+  const items = ['사과0', '바나나1', '딸기2'];
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App6</h1>
+      <ul>
+        {
+          items.map((item, index) => (
+            <ListItem key={index} text={item} />
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export function App7() {
+  const renderItems = [];
+  const items = ['사과', '바나나', '딸기'];
+  for (let i = 0; i < items.length; i++){
+    renderItems.push(<li key={i}>{items[i]}</li>);
+  }
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App7</h1>
+      <ul>
+        {renderItems}
+      </ul>
+    </div>
+  );
+}
+
+export function App8(){
+  const items = ['사과', '바나나', '딸기'];
+  const elements: React.ReactNode[] = [];
+  items.forEach((item, index) => {
+    elements.push(<li key={index}>{item}</li>);
+  });
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App8</h1>
+      <ul>
+        {elements}
+      </ul>
+    </div>
+  );
+}
+
+export function App9(){
+  const items = [
+    <li key='1'>사과</li>,
+    <li key='2'>바나나</li>,
+    <li key='3'>딸기</li>,
+  ];
+  return (
+    <div>
+      <hr style={{borderColor: 'red'}} />
+      <h1>App9</h1>
+      <ul>
+        {items.reduce<React.ReactNode[]>((acc, item) => {
+          acc.push(item);
+          return acc;
+        }, [])}
+      </ul>
+    </div>
+  );
+}
